9e6db358edd0981a62752d0ceb828574f1a01edd
Astin01 <wonhun1225@gmail.com>
2023-05-19 21:25:50 +0900

    feat : 메인 페이지 아웃라인

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,9 +6,25 @@ function Square({ value }: { value: string }) {
   }
 
   return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
+    <div>
+      <div>{/* navbar */}</div>
+      <div>
+        <div>The BnbScan Explorer</div>
+        <div>{/* search bar */}</div>
+      </div>
+      <div>
+        <div>{/* price price */}</div>
+        <div>{/* latest transaction and batch*/}</div>
+        <div>{/* 14 days history graph */}</div>
+      </div>
+      <div>
+        {/* latest L1 batch */}
+        {/* Latest transaction*/}
+        {/* Latest L1->L2 transaction */}
+        {/* Latest L2->L1 transaction */}
+      </div>
+      <div>{/* footer */}</div>
+    </div>
   );
 }
 
