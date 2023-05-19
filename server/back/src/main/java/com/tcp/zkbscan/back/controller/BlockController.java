c70ffdcb5e0347f14dcf59a73f2ff5fb0e122e03
김현욱 <company@wiselight.kr>
2023-05-20 02:36:09 +0900

    refactor: L1Block 관련 DTO클래스 내에 생성자 추가

--- server/back/src/main/java/com/tcp/zkbscan/back/controller/BlockController.java
+++ server/back/src/main/java/com/tcp/zkbscan/back/controller/BlockController.java
@@ -39,28 +39,7 @@ public class BlockController {
     public L1BlockDTO getL1BlockByNumber(@PathVariable("blockNumber") BigInteger blockNumber) {
         L1Block l1Block = l1BlockService.getBlockByNumber(blockNumber);
 
-        L1BlockDTO l1BlockDTO = new L1BlockDTO();
-        l1BlockDTO.setNumber(l1Block.getNumber());
-        l1BlockDTO.setDifficulty(l1Block.getDifficulty());
-        l1BlockDTO.setExtraData(l1Block.getExtraData());
-        l1BlockDTO.setGasLimit(l1Block.getGasLimit());
-        l1BlockDTO.setGasUsed(l1Block.getGasUsed());
-        l1BlockDTO.setHash(l1BlockDTO.getHash());
-        l1BlockDTO.setLogsBloom(l1Block.getLogsBloom());
-        l1BlockDTO.setMiner(l1BlockDTO.getMiner());
-        l1BlockDTO.setMixHash(l1Block.getMixHash());
-        l1BlockDTO.setNonce(l1Block.getNonce());
-        l1BlockDTO.setParentHash(l1Block.getParentHash());
-        l1BlockDTO.setReceiptsRoot(l1Block.getReceiptsRoot());
-        l1BlockDTO.setSha3Uncles(l1Block.getSha3Uncles());
-        l1BlockDTO.setSize(l1Block.getSize());
-        l1BlockDTO.setStateRoot(l1Block.getStateRoot());
-        l1BlockDTO.setTimestamp(l1Block.getTimestamp());
-        l1BlockDTO.setTotalDifficulty(l1Block.getTotalDifficulty());
-        l1BlockDTO.setTransactions(l1Block.getTransactions().stream().map(L1TransactionDTO::new).collect(Collectors.toList()));
-        l1BlockDTO.setTransactionsRoot(l1Block.getTransactionsRoot());
-
-        return l1BlockDTO;
+        return new L1BlockDTO(l1Block);
     }
 
     @Operation(summary = "L2 블록 조회", description = "BlockHeight 기준의 L2 블록 데이터를 조회합니다.")
--- server/back/src/main/java/com/tcp/zkbscan/back/dto/block/L1BlockDTO.java
+++ server/back/src/main/java/com/tcp/zkbscan/back/dto/block/L1BlockDTO.java
@@ -1,10 +1,12 @@
 package com.tcp.zkbscan.back.dto.block;
 
 import com.tcp.zkbscan.back.dto.transaction.L1TransactionDTO;
+import com.tcp.zkbscan.back.entity.L1Block;
 import lombok.Data;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class L1BlockDTO {
@@ -29,4 +31,25 @@ public class L1BlockDTO {
     private List<L1TransactionDTO> transactions;
     private String transactionsRoot;
 
+    public L1BlockDTO(L1Block l1Block) {
+        this.setNumber(l1Block.getNumber());
+        this.setDifficulty(l1Block.getDifficulty());
+        this.setExtraData(l1Block.getExtraData());
+        this.setGasLimit(l1Block.getGasLimit());
+        this.setGasUsed(l1Block.getGasUsed());
+        this.setHash(l1Block.getHash());
+        this.setLogsBloom(l1Block.getLogsBloom());
+        this.setMiner(l1Block.getMiner());
+        this.setMixHash(l1Block.getMixHash());
+        this.setNonce(l1Block.getNonce());
+        this.setParentHash(l1Block.getParentHash());
+        this.setReceiptsRoot(l1Block.getReceiptsRoot());
+        this.setSha3Uncles(l1Block.getSha3Uncles());
+        this.setSize(l1Block.getSize());
+        this.setStateRoot(l1Block.getStateRoot());
+        this.setTimestamp(l1Block.getTimestamp());
+        this.setTotalDifficulty(l1Block.getTotalDifficulty());
+        this.setTransactions(l1Block.getTransactions().stream().map(L1TransactionDTO::new).collect(Collectors.toList()));
+        this.setTransactionsRoot(l1Block.getTransactionsRoot());
+    }
 }
