0640c60c943ac2799ef540978ec0d52b10fc95a5
Astin01 <wonhun1225@gmail.com>
2023-05-19 22:34:19 +0900

    feat: priceBox 컴포넌트

---
+++ client/src/template/priceBox/priceBox.tsx
@@ -0,0 +1,27 @@
+import styles from "./priceBox.module.css";
+
+type ComponentProps = {
+  logo: string;
+  title: string;
+  price: number;
+  altprice: number;
+  flucrate: number;
+};
+
+function priceBox({ logo, title, price, altprice, flucrate }: ComponentProps) {
+  return (
+    <div>
+      <div className={styles.wrap}>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div>{title}</div>
+      <div>
+        <div>{price}</div>
+        <div>{altprice}</div>
+        <div>{flucrate}</div>
+      </div>
+    </div>
+  );
+}
+
+export default priceBox;
