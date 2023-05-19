674e65536e682f8a68c9b4ea63677111ac9e7d0a
Hyeongjin Lee <lhjin0j@gmail.com>
2023-05-19 21:51:29 +0900

    feat: create sidebar

---
+++ client/src/template/sidebar/nav.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+/** Component for NavigationSideBar Item: param(value: value of list; isActivate: is page active?) **/
+function Item({ value, isActivate }: { value: string; isActivate: boolean }) {
+  function handleOnClick() {
+    console.log(`item(${value}) of NavigationSideBar is Clicked!!`);
+  }
+
+  return (
+    <>
+      <li className={isActivate ? "active" : ""} onClick={handleOnClick}>
+        {value}
+      </li>
+    </>
+  );
+}
+
+/** Component for map Item Component: param(itemList: Object List of Item)**/
+export default function NavigationSideBar({
+  itemList,
+}: {
+  itemList: { name: string; isActivate: boolean }[];
+}) {
+  return (
+    <nav className="sidebar">
+      <div className="sidebar-header">
+        <h3>side bar header</h3>
+      </div>
+      <ul className="list-unstyled components">
+        {itemList.map((item) => (
+          <Item value={item.name} isActivate={item.isActivate} />
+        ))}
+      </ul>
+    </nav>
+  );
+}
