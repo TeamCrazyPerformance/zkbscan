d9d34eb80a3cf731214f09b1c45f45ae44d5bc4a
김현욱 <company@wiselight.kr>
2023-05-19 21:50:54 +0900

    feat: L1Block Jpa Repository

---
+++ server/back/src/main/java/com/tcp/zkbscan/back/repository/L1BlockRepository.java
@@ -0,0 +1,11 @@
+package com.tcp.zkbscan.back.repository;
+
+import com.tcp.zkbscan.back.entity.L1Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+
+@Repository
+public interface L1BlockRepository extends JpaRepository<L1Block, BigInteger> {
+}
