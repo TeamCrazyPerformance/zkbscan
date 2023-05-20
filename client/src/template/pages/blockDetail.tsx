c05f0a66941bb190fcf40d36ba12dbbb9e5ce3cd
Astin01 <wonhun1225@gmail.com>
2023-05-20 09:44:48 +0900

    fix : detail page 위치

---
+++ client/src/template/pages/blockDetail.tsx
@@ -0,0 +1,30 @@
+import Nav from "react-bootstrap/Nav";
+
+interface MyComponentProps {
+  data: {
+    [key: string]: string;
+  };
+}
+const blockDetail = ({ data }: MyComponentProps) => {
+  return (
+    <div>
+      <div className="wrapper">
+        <div></div>
+        <div>
+          <div>
+            <Nav variant="tabs" defaultActiveKey="/overview">
+              <Nav.Item>
+                <Nav.Link href="/overview">Overview</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="log">Log</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default blockDetail;
