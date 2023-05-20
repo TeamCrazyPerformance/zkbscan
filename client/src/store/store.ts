f8086f267161b8574bc825f4bd866346ddfce0f8
Astin01 <wonhun1225@gmail.com>
2023-05-20 15:10:17 +0900

    feat: useSearchhValue

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -8,8 +8,12 @@ interface NavListItem {
 interface AppState {
   navList: NavListItem[];
 }
-interface searchValue {
+interface SearchValue {
   searchValue: string;
+}
+interface ArticlState {
+  searchValue: string;
+  setSearchValue: (searchValue: string) => string;
 }
 
 export const useNavStore = create<AppState>()(
@@ -42,29 +46,11 @@ export const useNavStore = create<AppState>()(
     )
   )
 );
-export const useArticleState = create<AppState>()(
+export const useArticleState = create<ArticlState>()(
   devtools(
     persist(
       (set) => ({
-        navList: [
-          { name: "home", key: "/" },
-          { name: "Transactions", key: "trans" },
-          { name: "L1 -> L2 Transactions", key: "l1l2trans" },
-          { name: "L2 -> L1 Transactions", key: "l2l1trans" },
-          { name: "Contract internal transactions", key: "cit" },
-          { name: "L1 Transactions batches", key: "l1tb" },
-          { name: "View L1 state batches", key: "vl1sb" },
-          { name: "L2 Transactions", key: "l2t" },
-          { name: "L2 Transactions batches", key: "l2tb" },
-          { name: "View L2 state batches", key: "vl2sb" },
-          { name: "ERC 20 top tokens", key: "e2tt" },
-          { name: "ERC 20 transfer", key: "e2t" },
-          { name: "ERC 721 top tokens", key: "e7tt" },
-          { name: "ERC 721 transfer", key: "e7t" },
-          { name: "ERC 1155 top tokens", key: "e1tt" },
-          { name: "ERC 1155 transfer", key: "e1t" },
-          { name: "Daily transaction chart", key: "dtc" },
-        ],
+        increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
       }),
       {
         name: "navList",
@@ -72,11 +58,13 @@ export const useArticleState = create<AppState>()(
     )
   )
 );
-export const useSearchValue = create<searchValue>()(
+export const useSearchValue = create<SearchValue>()(
   devtools(
     persist(
       (set) => ({
         searchValue: "abc",
+        setSearchValue: (searchValue: string) =>
+          set((state) => ({ ...state, searchValue: searchValue })),
       }),
       {
         name: "searchValue",
