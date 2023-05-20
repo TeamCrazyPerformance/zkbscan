0a079bad2643f3f7b0bc870d283a5ffa6c059ef2
Astin01 <wonhun1225@gmail.com>
2023-05-21 02:00:22 +0900

    feat: chart 하나 완성

--- client/src/components/chart/chart.tsx
+++ client/src/components/chart/chart.tsx
@@ -1,78 +1,38 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
-import { ChartOptions } from "chart.js";
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from "victory";
 
-interface CoinData {
+interface IDataPoint {
   date: string;
   transactionCount: number;
   price: number;
 }
 
-interface ChartProps {
-  data: CoinData[];
-  xAxisLabel: string;
-  yAxisLabel: string;
+interface ILineChartProps {
+  data: IDataPoint[];
 }
 
-const ChartComponent: React.FC<ChartProps> = ({
-  data,
-  xAxisLabel,
-  yAxisLabel,
-}) => {
-  const formattedData = data.map((item) => ({
-    date: new Date(item.date),
-    transactionCount: item.transactionCount,
-    price: item.price,
-  }));
-
-  const labels = formattedData.map((item) =>
-    item.date.toISOString().slice(0, 10)
+const LineChart: React.FC<ILineChartProps> = ({ data }) => {
+  return (
+    <VictoryChart containerComponent={<VictoryVoronoiContainer />}>
+      <VictoryAxis dependentAxis tickFormat={(tick) => tick.toFixed(1)} />
+      <VictoryAxis tickFormat={(date) => new Date(date).toLocaleDateString()} />
+      <VictoryLine
+        data={data}
+        x="date"
+        y="transactionCount"
+        labelComponent={<VictoryTooltip />}
+        labels={({ datum }) =>
+          `Date: ${datum.date}\nTransaction Count: ${datum.transactionCount}\nPrice: ${datum.price}`
+        }
+      />
+    </VictoryChart>
   );
-  const transactionCounts = formattedData.map((item) => item.transactionCount);
-
-  const options: ChartOptions<"line"> = {
-    plugins: {
-      tooltip: {
-        callbacks: {
-          label: (tooltipItem): string => {
-            const index = tooltipItem.dataIndex;
-            const item = formattedData[index];
-            return `${item.date.toISOString().slice(0, 10)} | Transactions: ${
-              item.transactionCount
-            } | Price: ${item.price}`;
-          },
-        },
-      },
-    },
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: xAxisLabel,
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: yAxisLabel,
-        },
-      },
-    },
-  };
-
-  const chartDataConfig = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Transaction Count",
-        data: transactionCounts,
-        borderColor: "blue",
-        backgroundColor: "rgba(0, 0, 255, 0.2)",
-      },
-    ],
-  };
-
-  return <Line data={chartDataConfig} options={options} />;
 };
 
-export default ChartComponent;
+export default LineChart;
