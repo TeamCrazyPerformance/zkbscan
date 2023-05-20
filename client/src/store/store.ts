0977abcd3c37813700cda9c1b6b1c4e4f35c265a
Astin01 <wonhun1225@gmail.com>
2023-05-20 14:53:17 +0900

    feat : searchValue

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -8,6 +8,9 @@ interface NavListItem {
 interface AppState {
   navList: NavListItem[];
 }
+interface searchValue {
+  searchValue: string;
+}
 
 export const useNavStore = create<AppState>()(
   devtools(
@@ -35,6 +38,48 @@ export const useNavStore = create<AppState>()(
       }),
       {
         name: "navList",
+      }
+    )
+  )
+);
+export const useArticleState = create<AppState>()(
+  devtools(
+    persist(
+      (set) => ({
+        navList: [
+          { name: "home", key: "/" },
+          { name: "Transactions", key: "trans" },
+          { name: "L1 -> L2 Transactions", key: "l1l2trans" },
+          { name: "L2 -> L1 Transactions", key: "l2l1trans" },
+          { name: "Contract internal transactions", key: "cit" },
+          { name: "L1 Transactions batches", key: "l1tb" },
+          { name: "View L1 state batches", key: "vl1sb" },
+          { name: "L2 Transactions", key: "l2t" },
+          { name: "L2 Transactions batches", key: "l2tb" },
+          { name: "View L2 state batches", key: "vl2sb" },
+          { name: "ERC 20 top tokens", key: "e2tt" },
+          { name: "ERC 20 transfer", key: "e2t" },
+          { name: "ERC 721 top tokens", key: "e7tt" },
+          { name: "ERC 721 transfer", key: "e7t" },
+          { name: "ERC 1155 top tokens", key: "e1tt" },
+          { name: "ERC 1155 transfer", key: "e1t" },
+          { name: "Daily transaction chart", key: "dtc" },
+        ],
+      }),
+      {
+        name: "navList",
+      }
+    )
+  )
+);
+export const useSearchValue = create<searchValue>()(
+  devtools(
+    persist(
+      (set) => ({
+        searchValue: "abc",
+      }),
+      {
+        name: "searchValue",
       }
     )
   )
