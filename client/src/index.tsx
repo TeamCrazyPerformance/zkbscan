05600430b7238ea9d5fb60b44e665fc7e449c964
Hyeongjin Lee <lhjin0j@gmail.com>
2023-05-20 09:22:21 +0900

    update

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
